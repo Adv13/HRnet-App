f05e63a79491a9b464b20ace836f0daa3891898e
Adv <tiffanydpro923@gmail.com>
2022-07-07 19:22:02 +0200

    maj home file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,9 +6,54 @@ function Home(){
         
         <main>
             <Header/>
-            <div>
-                <h2>Body test</h2>
+            <div class="container">
+            <h1 className="title">Create Employee</h1>
+            <div className="info">
+                <form action="#" className="form" id="create-employee">
+                    <label for="first-name">First Name</label>
+                    <input type="text" id="first-name"></input>
+
+                    <label for="last-name">Last Name</label>
+                    <input type="text" id="last-name"></input>
+
+                    <label for="date-of-birth">Date of Birth</label>
+                    <input id="date-of-birth" type="text"></input>
+
+                    <label for="start-date">Start Date</label>
+                    <input id="start-date" type="text"></input>
+                </form>
             </div>
+            
+            <form>
+                <fieldset className="address">
+                    <legend>Address</legend>
+
+                    <label for="street">Street</label>
+                    <input id="street" type="text"></input>
+
+                    <label for="city">City</label>
+                    <input id="city" type="text"></input>
+
+                    <label for="state">State</label>
+                    <select name="state" id="state"></select>
+
+                    <label for="zip-code">Zip Code</label>
+                    <input id="zip-code" type="number"></input>
+                </fieldset>
+
+                <label for="department">Department</label>
+                <select name="department" id="department">
+                    <option>Sales</option>
+                    <option>Marketing</option>
+                    <option>Engineering</option>
+                    <option>Human Resources</option>
+                    <option>Legal</option>
+                </select>
+            </form>
+
+            <button onclick="saveEmployee()">Save</button>
+        </div>
+        <div id="confirmation" class="modal">Employee Created!</div>
         </main>
     )
 }
